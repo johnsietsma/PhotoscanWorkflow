4f2a226987a09830370c57c44343e5e3229e0e09
John Sietsma <johns@aie.edu.au>
2015-10-06 09:57:48 +1100

    - Added WorkflowJob class which encapsulates a job. - Added config options for each job. - Export files now go in a separate export folder. - Fixed precondition for camera alignment.

--- workflow.py
+++ workflow.py
@@ -10,6 +10,83 @@ A workflow to process multiple set of photos in Photoscan.
 See the bottom of the file for configuration options and documentation.
 '''
 
+###########################################################################################################
+
+'''
+Expects a directory structure like this:
+<HomeDirectory>
+	Directory1
+		<PhotosDirectory>
+			photo1.tif
+			photo2.tif
+	Directory2
+		<PhotosDirectory>
+			photo1.tif
+			photo2.tif		
+
+It will put a PhotoScan project file in the base of each directory named <directory>.psz.
+All the photos from the <PhotosPdirectory> will be added to the project.
+
+The workflow produces an fbx and texture for each directory, which will be placed in the root
+of the directory.
+
+This script can be run multiple times to continue processing after a crashed or cancelled processing run. It
+will skip stages that have already been completed.
+'''
+
+## Configuration
+
+HomeDirectory = "E:\\Captures"
+PhotosDirectory = "Photos"
+ExportDirectory = "Export"
+
+match_photos_config = {
+	'accuracy': PhotoScan.HighAccuracy,
+	'preselection': PhotoScan.GenericPreselection,
+}
+
+
+build_point_cloud_config = {}
+
+
+build_dense_cloud_config = {
+	'quality': PhotoScan.HighQuality
+}
+
+build_model_config = {
+	'surface': PhotoScan.Arbitrary, 
+	'interpolation': PhotoScan.EnabledInterpolation
+}
+
+export_model_config = {
+	'binary': True,
+	'texture_format': "jpg",
+	'texture': True, 
+	'normals': True, 
+	'colors': True,
+	'cameras': False,
+	'format': "fbx"
+}
+
+build_texture_config = {}
+
+#################################################
+
+
+class WorkflowJob(object):
+	def __init__(self, name, precond_func, run_func):
+		self.name = name
+		self.precond_func = precond_func
+		self.run_func = run_func
+
+	def can_run(self, chunk):
+		return self.precond_func(chunk)
+
+	def run(self, chunk):
+		log( "Running job: " + self.name )
+		return self.run_func(chunk)
+
+
 
 #################################################
 # Logging
@@ -35,13 +112,6 @@ def close_log():
 def log_time():
 	log( datetime.datetime.utcnow() )
 
-def log_chunk_data(chunk):
-	log("Number of cameras: {}".format(len(chunk.cameras)))
-	log("Cameras are aligned: {}".format(are_cameras_aligned(chunk)))
-	log("Has sparse cloud: {}".format(chunk.point_cloud is not None))
-	log("Has dense cloud: {}".format(chunk.dense_cloud is not None))
-	log("Has model: {}".format(chunk.model is not None))
-
 
 #################################################
 # Utility functions
@@ -49,22 +119,53 @@ def log_chunk_data(chunk):
 def does_project_exist(project_dir):
 	'Does the PhotoScan project file exist'
 	project_name = make_project_filename(project_dir, "psz")
-	return glob.glob(project_dir + "\\*.psz") is not None	
+	return glob.glob(project_dir + "\\*.psz")
 
 def are_cameras_aligned(chunk):
-	return len([c for c in chunk.cameras if c.center is not None]) == len(chunk.cameras)
+	'Assume cameras are aligned if at least one of them have been moved.'
+	return len([c for c in chunk.cameras if c.center is not None]) > 0
+
+
+def export_file_exists(project_directory, format):
+	model_name = make_export_filename(project_directory, format)
+	return os.path.isfile( model_name )
+
 
 def is_valid_project_dir( project_dir, photos_dir ):
 	"A valid project directory has a photos subdirectory."
-	return photos_dir in os.listdir(project_dir)
+	return photos_dir.upper() in (d.upper() for d in os.listdir(project_dir))
+
 
 def estimate_image_quality(photo):
 	return Photoscan.Utils.estimateImageQuality(photo)
+
+
+def get_export_path(project_dir):
+	dir_name = os.path.basename(project_dir)
+	return os.path.join(project_dir, ExportDirectory)
+
 
 def make_project_filename(project_dir, ext):
 	'Make a filename in the project directory with the given extension'
 	dir_name = os.path.basename(project_dir)
 	return os.path.join(project_dir,dir_name + "." + ext);
+
+
+def make_export_filename(project_dir, ext):
+	"Make a filename in the project's export directory"
+	dir_name = os.path.basename(project_dir)
+	export_path = get_export_path(project_dir)
+	return os.path.join(export_path, dir_name + "." + ext);
+
+
+def export_model(chunk, project_directory, kwargs):
+	model_name = make_export_filename(project_directory, kwargs['format'])
+	return chunk.exportModel(model_name, **kwargs)
+
+
+def export_texture(chunk, project_directory):
+	texture_name = make_export_filename(project_directory, "jpg")
+	return chunk.model.saveTexture(texture_name)
 
 
 #################################################
@@ -80,6 +181,14 @@ def dump_camera_data(cam, fp):
 	dump_meta(cam.meta, fp)
 	fp.write("Photo Path: " + cam.photo.path + "\n")
 	fp.write("Center: " + str(cam.center) + "\n")
+
+
+def log_chunk_data(chunk):
+	log("Number of cameras: {}".format(len(chunk.cameras)))
+	log("Cameras are aligned: {}".format(are_cameras_aligned(chunk)))
+	log("Has sparse cloud: {}".format(chunk.point_cloud is not None))
+	log("Has dense cloud: {}".format(chunk.dense_cloud is not None))
+	log("Has model: {}".format(chunk.model is not None))
 
 
 def dump_chunk_data(chunk, fp):
@@ -122,17 +231,18 @@ def find_project_folders(home_dir, photos_dir):
 
 
 def open_project(project_dir):
+	log( "Opening project " + project_dir )
 	project_name = make_project_filename(project_dir, "psz")
-
 	doc = PhotoScan.Document()
 	if not doc.open( project_name ):
 		log( "ERROR: Cold not open document: " + project_name)
-
 	return doc
 
 
 def make_project(project_dir, photos_dir):
 	'Make a new project and add the photos from the photos_dir to it.'
+
+	log( "Making project " + project_dir )
 
 	# Create new doc
 	doc = PhotoScan.Document()
@@ -162,125 +272,100 @@ def make_or_open_project(project_dir, photos_dir):
 		return make_project(project_dir, photos_dir)
 
 
-def make_or_open_projects(project_dirs, photos_dir):
-	return [make_or_open_project(pd, photos_dir) for pd in project_dirs]
-
-
-def build_chunk(chunk, doc):
-	log( "Building chunk" )
+def build(project_dir, photos_dir, jobs):
+	log( "Building document: " + project_dir )
 	log_time()
+
+	doc = make_or_open_project(project_dir, photos_dir)
+
+	export_path = get_export_path(project_dir)
+	if not os.path.exists(export_path):
+		os.makedirs(export_path)
+
+	chunk = doc.chunk
+
+	if not chunk:
+		log( "ERROR: Chunk is None")
+		return False
 
 	if not chunk.enabled:
 		log( "Chunk not enabled, skipping" )
 
-	if not are_cameras_aligned(chunk):
-		chunk.matchPhotos(accuracy=PhotoScan.HighAccuracy, preselection=PhotoScan.GenericPreselection)
-		if not chunk.alignCameras():
-			log( "ERROR: Could not align cameras" )
-			return False
+	# For use by jobs
+	chunk.label = project_dir
+
+	ret = True
+	for job in jobs:
+		if job.can_run( chunk ):
+			if job.run( chunk ):
+				log( "Job finished - " + job.name )
+				doc.save()
+			else:
+				log( "ERROR: Job failed - " + job.name )
+				ret = False
+				break
 		else:
-			doc.save()
-	else:
-		log( "Cameras are already aligned.")
+			log( "Skipping job " + job.name )
 
-
-	if not chunk.point_cloud:
-		if not chunk.buildPoints():
-			log( "ERROR: Could not build sparse cloud" )
-			return False
-		else:
-			doc.save()
-
-	if not chunk.dense_cloud:
-		if not chunk.buildDenseCloud(quality=PhotoScan.HighQuality):
-			log( "ERROR: Could not build dense cloud" )
-			return False
-		else:
-			doc.save()
-	else:
-		log( "Dense cloud already exists." )
-
-
-	if not chunk.model:
-		if not chunk.buildModel(surface=PhotoScan.Arbitrary, interpolation=PhotoScan.EnabledInterpolation):
-			log( "ERROR: Could not build model")
-			return False
-		else:
-			doc.save()
-	else:
-		log( "Model already exists" )
-
-	if not chunk.model and not chunk.model.texture:
-		if not chunk.buildTexture():
-			log( "ERROR: Could not build texture")
-			return False
-		else:
-			doc.save()
-	else:
-		log( "Texture already exists" )
 
 	log( "Finished building chunk" )
-
-	return True
-
-
-def build(doc):
-	log( "Building " + doc.path )
 	log_time()
-	if not build_chunk( doc.chunk, doc ):
-		return False
-	doc.save()
-	log( "Finished building" )
-	return True
 
-
-def export(chunk, project_directory):
-	log( "Exporting chunk" )
-
-	model_name = make_project_filename(project_directory, "fbx")
-
-	if not os.path.isfile( model_name ):
-		if not chunk.exportModel(model_name, binary=True, texture_format="jpg", texture=True, normals=True, colors=True, cameras=False, format="fbx"):
-			log( "ERROR: Could not export model " + model_name)
-	else:
-		log( "Model exists, skipping" )
-
-
-def build_and_export(project_dir, photos_dir):
-	doc = make_or_open_project(project_dir, photos_dir)
-	if build(doc):
-		dump(doc)
-		export( doc.chunk, project_dir )
-	else:
-		log( "ERROR: Build failed." )
+	return ret
 
 
 ###########################################################################################################
+# Workflow jobs
 
-'''
-Expects a directory structure like this:
-<HomeDirectory>
-	Directory1
-		<PhotosDirectory>
-			photo1.tif
-			photo2.tif
-	Directory2
-		<PhotosDirectory>
-			photo1.tif
-			photo2.tif		
+match_photos_job = WorkflowJob(
+		"Match photos",
+		lambda chunk: not are_cameras_aligned(chunk),
+		lambda chunk: chunk.matchPhotos(**match_photos_config)
+	)
 
-It will put a PhotoScan project file in the base of each directory named <directory>.psz.
-All the photos from the <PhotosPdirectory> will be added to the project.
+align_cameras_job = WorkflowJob(
+		"Align Cameras", 
+		lambda chunk: not are_cameras_aligned(chunk),
+		lambda chunk: chunk.alignCameras
+	)
 
-The workflow produces an fbx and texture for each directory, which will be placed in the root
-of the directory.
+build_point_cloud_job = WorkflowJob(
+		"Build point cloud", 
+		lambda chunk: not chunk.point_cloud,
+		lambda chunk: chunk.buildPoints(**build_point_cloud_config)
+	)
 
-This script can be run multiple times to continue processing after a crashed or cancelled processing run. It
-will skip stages that have already been completed.
-'''
+build_dense_cloud_job = WorkflowJob(
+		"Build dense cloud", 
+		lambda chunk: not chunk.dense_cloud,
+		lambda chunk: chunk.buildDenseCloud(**build_dense_cloud_config)
+	)
 
-HomeDirectory = "E:\\Captures"
-PhotosDirectory = "photos"
+build_model_job = WorkflowJob(
+		"Build model", 
+		lambda chunk: not chunk.model,
+		lambda chunk: chunk.buildModel(**build_model_config)
+	)
+
+build_texture_job = WorkflowJob(
+		"Build texture", 
+		lambda chunk: not chunk.model.texture(),
+		lambda chunk: chunk.buildTexture(**build_texture_config)
+	)
+
+export_model_job = WorkflowJob(
+		"Export model", 
+		lambda chunk: chunk.model and not export_file_exists(chunk.label, export_model_config['format']),
+		lambda chunk: export_model( chunk, chunk.label, export_model_config )
+	)
+
+export_texture_job = WorkflowJob(
+		"Export texture", 
+		lambda chunk: chunk.model.texture() and not export_file_exists(chunk.label, export_model_config['texture_format']),
+		lambda chunk: export_texture( chunk, chunk.label )
+	)
+
+###########################################################################################################
 
 open_log( HomeDirectory )
 
@@ -293,8 +378,22 @@ project_dirs = find_project_folders(HomeDirectory, PhotosDirectory)
 log( "Found {} project directories".format( len(project_dirs) ) )
 
 log( "Making projects" )
-[build_and_export(pd, PhotosDirectory) for pd in project_dirs]
+
+jobs = (
+		match_photos_job,
+		align_cameras_job, 
+		build_point_cloud_job,
+		build_dense_cloud_job,
+		build_model_job,
+		build_texture_job,
+		export_model_job,
+		export_texture_job
+	)
+
+[build(pd, PhotosDirectory, jobs) for pd in project_dirs]
 
 log_time()
 log( "--- Finished workflow ---")
 close_log()
+
+
